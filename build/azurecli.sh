0e30684c690bc3fd701504a16d19831c1f37cda1
mlarned <mlarned@gmail.com>
2019-02-23 20:53:58 -0700

    Added terraform endpoint setup

--- build/azurecli.sh
+++ build/azurecli.sh
@@ -3,15 +3,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+MS_KEYRING="/etc/apt/trusted.gpg.d/Microsoft.gpg"
+MS_KEYSERVER="packages.microsoft.com"
+MS_KEY="BC528686B50D79E339D3721CEB3E94ADBE1229CF"
 
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get install apt-transport-https lsb-release software-properties-common -y
 
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-     --keyserver packages.microsoft.com \
-     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+AZ_REPO=$(lsb_release -cs) 
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
+sudo apt-key --keyring $MS_KEYRING adv --keyserver $MS_KEYSERVER --recv-keys $MS_KEY
 sudo apt-get update
-sudo apt-get install azure-cli 
+sudo apt-get install azure-cli
