184f1ec17ec5a7fdf9915930ead53d7dfbd92f8f
mlarned <mlarned@gmail.com>
2019-02-25 08:54:11 -0700

    Storage client

---
+++ build/storage.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+AZURE_STORAGE_ACCOUNT=""
+AZURE_STORAGE_KEY=""
+CONTAINER_NAME="endpointdemo"
+BLOB_NAME=$(cat /proc/sys/kernel/random/uuid)
+FILE="$BLOB_NAME.txt"
+
+if [ -z "$1" ]
+  then
+    echo "Arg1: Account Name for Azure Blob Storage Required"
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Arg2: Account Key for Azure Blob Storage Required"
+fi
+
+AZURE_STORAGE_ACCOUNT=$1
+AZURE_STORAGE_KEY=$2
+
+echo "$BLOB_NAME" > $FILE
+az storage container create --name $CONTAINER_NAME --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT" --output none
+
+echo ""
+echo "[UPLOAD] => $FILE"
+echo ""
+az storage blob upload --container-name $CONTAINER_NAME --file $FILE --name $FILE --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT"
+echo ""
+
+echo "[LIST BLOBS]"
+echo ""
+az storage blob list --container-name $CONTAINER_NAME --output table --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT"
+echo ""
